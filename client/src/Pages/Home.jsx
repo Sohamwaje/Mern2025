6fdb0f37ec9af77034742f39c7d6dfc19075dbc0
Sohamwaje <sohammwaje@gmail.com>
2025-01-22 15:06:04 +0530

    Home page added

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,9 +1,23 @@
 //import { Navbar } from "../components/Navbar";
+import React from "react";
+import "./Home.css";
+
 
 
 export const Home = ()=>{
-    return <>
+    return (
+    <>
  {/* <Navbar/> */}
-    <h1>Hello Home page</h1>
+ <div className="hero-section">
+      <img src="/images/Logo.png" alt="screenshot" />
+      <h1>Welcome to Omega Technologies</h1>
+      <p>Choose the service you need to get started:</p>
+      <div className="buttons">
+        <button className="hero-btn">New Installation</button>
+        <button className="hero-btn">Repair</button>
+        <button className="hero-btn">Support</button>
+      </div>
+    </div>
     </>
+    )
 };
